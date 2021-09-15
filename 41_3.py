75eb5bc4b7a111646b4a88b9a9142e37d03908e6
Taein2 <dls102s@gmail.com>
2021-09-16 01:27:49 +0900

    algebra numpy exercise

---
+++ 41_3.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+A = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+
+v = np.array([[1],
+              [2],
+              [3]])
+
+print("A =", A)
+print("v =", v)
+
+w = np.array([1,2,3])
+print("w =", w)
+print("w.shape =", w.shape)
+
+B = np.array([[1,2,3], [4,5,6]])
+print("B = ", B)
+print("B.shape =", B.shape)
